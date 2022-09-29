3e0ef7c316eb157eaef62a950580f70aa3b20441
maurocardia <maurocardia27@gmail.com>
2022-09-28 23:38:31 -0500

    sjdfsjfkljd

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ const startServer = async () => {
 		const PORT = process.env.PORT || 4000;
 
 		app.listen(PORT, () => {
-			console.log('Express app running!');
+			console.log('Express app running!', PORT);
 		});
 	} catch (error) {
 		console.log(error);
